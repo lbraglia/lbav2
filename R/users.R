49da2387e3455b6c987a1fa991b36cede2d24a27
Luca Braglia <lbraglia@gmail.com>
2021-05-06 12:02:26 +0200

    update

--- R/users.R
+++ R/users.R
@@ -1,5 +1,9 @@
 initialize <- function(users_f) {
-    private$data <- utils::read.csv(users_f, comment = '#')
+    pubdata <- utils::read.csv(users_f, comment = '#')
+    privdata <- lbprivee::av_yt_users
+    data <- merge(pubdata, privdata, by = 'gh_user', all.x = TRUE)
+    data$nomec <- paste(data$nome, strtrim(data$cognome, 1), sep = "")
+    private$data <- data
 }
 
 #' @export
@@ -23,13 +27,23 @@ users <- R6::R6Class('users',
                              print(private$data, row.names = FALSE)
                              ## cat("```\n")
                          },
+                         ## per il gruppo telegram, usare tg_user
                          mention = function(role = c('translator', 'revisor1', 'revisor2')){
+                             tmp <- private$data
                              role <- match.arg(role)
-                             who <- switch(role,
-                                           'translator' = self$translators(),
-                                           'revisor1' = self$revisors1(),
-                                           'revisor2' = self$revisors2())
-                             cat(sprintf("@%s", who), sep = ' ')
+                             if (role == 'translator') {
+                                 index <- tmp$translator
+                             } else if (role == 'revisor1') {
+                                 index <- tmp$revisor1
+                             } else if (role == 'revisor2') {
+                                 index <- tmp$revisor2
+                             } else stop('WTF, why are you here?')
+                             
+                             select <- tmp[index, ]
+                             who <- with(select,
+                                         ifelse(!is.na(tg_user), tg_user, nome)
+                                         )
+                             cat(sprintf("@%s", sort(who)), sep = ' ')
                          }
                      ),
                      private = list(
